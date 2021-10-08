3f7229c1bae76646167c1e3ed4ea497ae8e3dc95
Jan Peter <44668286+JanPeterChristiansen@users.noreply.github.com>
2021-10-08 14:12:03 +0200

    Update day7.py

--- day7/day7.py
+++ day7/day7.py
@@ -1,14 +1,58 @@
 import re
 
 file = open("input.txt","r")
-lines = file.read().split("\n")
+lines = file.read().replace("no other bags","").split("\n")
 del lines[-1]
 
-pattern = re.compile("(\d )?(\w+ \w+) bags")
+pattern = re.compile("(\w+ \w+) bags?")
 rules = [re.findall(pattern, rule) for rule in lines]
 
-valid_bags = []
-for rule in rules:
-    if any("shiny gold" in color[1] for color in rule):
-        valid_bags.append(rule[0][1])
+start_points = [rule[0] for rule in rules]
+
+
+def contains_shiny_gold(rule):
+    if len(rule)==1:
+        #reached an endpoint
+        return 0
+    elif rule[0]=="shiny gold":
+        #a shiny golden cannot contain another shiny golden
+        return 0
+    else:
+        if "shiny gold" in rule:
+            #bag contains a shiny golden bag
+            return 1
+        else:
+            #bag does not contain a shiny golden bag
+            for bag in rule[1:]:
+                #check the contained bag if they contain a shiny golden instead
+                indx = start_points.index(bag)
+                if contains_shiny_gold(rules[indx]):
+                    return 1
+
+            return 0
+
+cnt = 0
+for i, rule in enumerate(rules):
+    if contains_shiny_gold(rule):
+        cnt += 1
+
+
+
+pattern2 = re.compile("(\d)? ?(\w+ \w+) bags?")
+rules2 = [re.findall(pattern2,rule) for rule in lines]
+
+def nr_of_bags(rule):
+    if len(rule)==1:
+        return 0
+    else:
+        summe = 0
+        for bag in rule[1:]:
+            indx = start_points.index(bag[1])
+            summe += int(bag[0])+int(bag[0])*nr_of_bags(rules2[indx])
+        return summe
+
+
+cnt2 = 0
+indx = start_points.index("shiny gold")
+cnt2 = nr_of_bags(rules2[indx])
 
