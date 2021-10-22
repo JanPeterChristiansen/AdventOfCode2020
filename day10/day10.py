ece5e260e339cf716cb65883f6756706428f3617
Jan Peter <44668286+JanPeterChristiansen@users.noreply.github.com>
2021-10-22 17:29:30 +0200

    5head solution for part II

--- day10/day10.py
+++ day10/day10.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 file = open("input.txt", "r")
 adapters = file.read().split("\n")[: -1]
 adapters = [int(val) for val in adapters]
@@ -5,8 +7,24 @@ adapters.sort()
 adapters.insert(0, 0)
 adapters.append(max(adapters)+3)
 
-diff = [adapters[i+1]-adapters[i] for i in range(len(adapters)-1)]
 
-one_jolt = diff.count(1)
-three_jolt = diff.count(3)
-print("Answer to part I: "+str(one_jolt*three_jolt))
+# %%
+
+
+difference = [adapters[i+1]-adapters[i] for i in range(len(adapters)-1)]
+print("Answer to part I: "+str(difference.count(1)*difference.count(3)))
+
+# %% Copied from viliampucik
+# https://github.com/viliampucik/adventofcode/blob/master/2020/10.py
+
+# the only way to reach the next adapter is by using one that has a value
+# at most 3 less than the one you try to reach.
+
+# thus make an ordered list of ways to reach each value, starting at the first value
+# the way to reach the next adapter is the sum of the ways to reach the previous 3
+
+counts = defaultdict(int, {0: 1})
+for a in adapters[1:]:
+    counts[a] = counts[a - 3] + counts[a - 2] + counts[a - 1]
+
+print("Answer to part II: "+str(counts[adapters[-1]]))
